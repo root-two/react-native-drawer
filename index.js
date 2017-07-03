145db3e62020ab0e24a14b362a2f23a28afebc93
sean-macfarlane <sean.g.macfarlane@gmail.com>
2017-07-03 15:07:57 -0400

    fix to work with React 16.0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import { PanResponder, View, StyleSheet, Dimensions, InteractionManager, I18nManager } from 'react-native'
 
 import tween from './tweener'
