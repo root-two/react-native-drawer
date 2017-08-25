b7c5e9989af5908bb11d9a5752069a0af884adf6
Tuyen Nguyen <tuyennguyen266@users.noreply.github.com>
2017-08-26 01:01:10 +0700

    Add ability to pan on the drawer view when it is 'open'. (#270)
    * Add ability to pan on the drawer view when it is 'open'.
    
    * Lock panning outside of mask when drawer 'closed'

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ export default class Drawer extends Component {
     acceptDoubleTap: PropTypes.bool,
     acceptPan: PropTypes.bool,
     acceptTap: PropTypes.bool,
+    acceptPanOnDrawer: PropTypes.bool,
     captureGestures: PropTypes.oneOf([true, false, 'open', 'closed']),
     children: PropTypes.node,
     closedDrawerOffset: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
@@ -91,6 +92,7 @@ export default class Drawer extends Component {
     acceptDoubleTap: false,
     acceptTap: false,
     acceptPan: true,
+    acceptPanOnDrawer: true,
     tapToClose: false,
 
     styles: {},
@@ -307,7 +309,7 @@ export default class Drawer extends Component {
 
   processMoveShouldSet = (e, gestureState) => {
     let inMask = this.testPanResponderMask(e, gestureState)
-    if (!inMask) return false
+    if (!inMask && (!this.props.acceptPanOnDrawer || this._open === false )) return false
     if (!this.props.acceptPan) return false
 
     if (!this.props.negotiatePan || this.props.disabled || !this.props.acceptPan || this._panning) return false
